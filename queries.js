0f674d74d909babe2f19d177720f5f26245585a7
daneparke <dapa8522@colorado.edu>
2018-12-06 10:57:26 -0700

    added all query routes

---
+++ queries.js
@@ -0,0 +1,19 @@
+const db = require('./database-connection')
+module.exports = {
+    getAllMovies() {
+        return db('movies')
+    },
+    getMovieById(id) {
+        return db('movies').where({ id: id }).first()
+    },
+    createMovie(newMovie) {
+        return db('mobies').insert(newMovie).returning('*')
+    },
+    deleteMovie(id) {
+        return db('movies').where({ id: id }).delete()
+    },
+    updateMovie(id, movie) {
+        return db('movies').where({ id: id }).update(movie).returning('*')
+    }
+
+}
