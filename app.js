6f4395c4c8c829ebc7290559500f848373e72080
daneparke <dapa8522@colorado.edu>
2018-12-06 11:03:43 -0700

    added post, delete, get by id and get all in app, now lets see if they work

--- app.js
+++ app.js
@@ -7,6 +7,36 @@ const bodyParser = require('body-parser')
 
 app.use(cors())
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+app.get('/', (req, res) => {
+    queries.getAllMovies().then(movies => res.status(200).send({ movies }))
+})
+
+app.get('/:id', (req, res) => {
+    queries.getMovieById(req.params.id).then(movie => res.status(200).send(student))
+})
+
+app.post('/', (req, res) => {
+    queries.createMovie(req.body).then(newMovie => res.status(201).send(newMovie))
+})
+
+app.delete('/:id', (req, res) => {
+    queries.deleteMovie(id).then(res.status(204))
+})
+
+app.use((req, res, next) => {
+    res.status(404).json({ message: 'Not found.' })
+})
+
+app.use((err, req, res, next) => {
+    const status = err.status || 500
+    res.status(status).json({ error: err })
+})
+
+
+
 
 const listener = () => console.log(`Listening on port ${port}`)
 app.listen(port, listener)
